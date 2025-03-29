d05cba255c46df360462144985fb191e529b2139
KizunaG <hecto@LAPTOP-IUQ31NPB>
2025-03-28 22:02:36 -0600

    Se agregó el archivo JS que controla el tablero

---
+++ Gestión de tareas/wwwroot/js/tablero.js
@@ -0,0 +1,21 @@
+﻿function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var tareaElement = document.getElementById(data);
+    ev.target.appendChild(tareaElement);
+
+    // Extraer ID y nuevo estado
+    var tareaId = data.split("-")[1];
+    var nuevoEstado = ev.target.id === "pendiente" ? "Pendiente" : "Completa";
+
+    // Llamar a Blazor para actualizar la base de datos
+    DotNet.invokeMethodAsync('GestionTareasApp', 'CambiarEstadoTarea', parseInt(tareaId), nuevoEstado);
+}
